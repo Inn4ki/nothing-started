27dbe9960e0ed6e4e219978c83043f61b991e38d
Inn4ki <ilacaci@gmail.com>
2017-02-14 19:20:51 +0100

    2nd commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var static = require("./static/static.js");
+static = require("./static.js");
 
 http.createServer(function webRequestHandler(request, response){
     console.log(`Incoming request: ${request.url}`);
